fcc943557042b5c8d3ad9599ab48652a3a8858c6
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-11-24 01:33:00 -0300

    refactor: remover DTOs, entidades e migrações não utilizadas para usuários e relatórios

--- src/application/relatorio/update-relatorio.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger'; 
-import { CreateRelatorioDto } from './create-relatorio.dto';
-
-export class UpdateRelatorioDto extends PartialType(CreateRelatorioDto) {}
