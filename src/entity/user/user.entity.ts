37151461c65f32529b55c19a3e2d12e07c5190ce
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-11-24 01:37:42 -0300

    feat(entity): Atualiza UserEntity com campos adicionais

--- src/entity/user/user.entity.ts
+++ src/entity/user/user.entity.ts
@@ -1,25 +1,47 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from 'typeorm';
+import { 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  CreateDateColumn, 
+  UpdateDateColumn, 
+  OneToMany 
+} from 'typeorm';
 import { Reembolso } from '../reembolso/reembolso.entity';
+import { UserRole } from './user.enums'; 
+
 @Entity('usuario')
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid', { name: 'id_usuario' })
   id: string;
 
-  @Column({ name: 'nome', length: 100 })
+  @Column({ name: 'nome', type: 'varchar', length: 255 }) 
   nome: string;
 
-  @Column({ name: 'email', unique: true, length: 100 })
+  @Column({ name: 'email', type: 'varchar', unique: true, length: 255 })
   email: string;
   
-  // select: false, que é para esconder a senha no retorno
-  @Column({ name: 'senha', select: false }) 
+  @Column({ name: 'senha', type: 'varchar', select: false, length: 255 }) 
   senha: string;
+
+  @Column({
+    name: 'role',
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.MEMBER, 
+  })
+  role: UserRole;
 
   @Column({ name: 'ativo', default: true })
   ativo: boolean;
 
+  @Column({ name: 'avatar_url', type: 'varchar', length: 255, nullable: true })
+  avatarUrl: string;
+
   @CreateDateColumn({ name: 'data_criacao' })
   dataCriacao: Date;
+
+  @UpdateDateColumn({ name: 'data_atualizacao' }) 
+  dataAtualizacao: Date;
 
   @OneToMany(() => Reembolso, (reembolso) => reembolso.usuario)
   reembolsos: Reembolso[];
