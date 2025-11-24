0383eab96ff8e6d4bcb182c4bdaf2676f3521562
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-11-24 13:15:05 -0300

    feat(entity): adiciona entidade Venda com propriedades e enums

---
+++ src/entity/venda/venda.enums.ts
@@ -0,0 +1,15 @@
+export enum CategoriaVenda {
+  CONSULTORIA = 'Serviços Consultoria',
+  LICENCAS = 'Licenças Software',
+  SUPORTE = 'Suporte Técnico',
+  CUSTOMIZADO = 'Desenvolvimento Customizado',
+  IMPLANTACAO = 'Implantação Sistema',
+  TREINAMENTO = 'Treinamento',
+}
+
+export enum StatusVenda {
+  CONCLUIDA = 'Concluida',
+  PENDENTE = 'Pendente',
+  CANCELADA = 'Cancelada',
+  PROCESSANDO = 'Processando',
+}
