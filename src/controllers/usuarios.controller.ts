2453c88049f36d82e11ed116ee6dd7787f309f5e
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-11-25 13:36:21 -0300

    feat: remove comentários desnecessários

--- src/controllers/usuarios.controller.ts
+++ src/controllers/usuarios.controller.ts
@@ -8,28 +8,26 @@ import {
   Delete, 
   HttpCode, 
   HttpStatus,
-  UseGuards // <--- Importe
+  UseGuards 
 } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBearerAuth } from '@nestjs/swagger';
 import { UsuariosService } from '../application/user/usuarios.service';
 import { CriarUsuarioDto } from '../application/user/dto/create-user.dto';
 import { AtualizarUsuarioDto } from '../application/user/dto/update-user.dto';
-
-// Imports de Segurança
 import { JwtAuthGuard } from '../application/auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../application/auth/guards/roles.guard';
 import { Roles } from '../application/auth/decorators/roles.decorator';
 import { UserRole } from '../entity/user/user.enums';
 
 @ApiTags('Usuários') 
-@ApiBearerAuth() // <--- Adiciona o cadeado no Swagger
+@ApiBearerAuth() 
 @Controller('usuarios')
-@UseGuards(JwtAuthGuard, RolesGuard) // <--- Protege TUDO com Login + Validação de Roles
+@UseGuards(JwtAuthGuard, RolesGuard) 
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
   @Post()
-  @Roles(UserRole.ADMIN) // Só ADMIN cria usuário
+  @Roles(UserRole.ADMIN) 
   @ApiOperation({ summary: 'Cria um novo usuário (Apenas Admin)' })
   @ApiResponse({ status: 201, description: 'Usuário criado com sucesso.' })
   @ApiResponse({ status: 403, description: 'Proibido (Apenas Admin).' })
@@ -38,7 +36,6 @@ export class UsuariosController {
   }
 
   @Get()
-  // Se não colocar @Roles, qualquer usuário logado (Admin ou Member) pode acessar
   @ApiOperation({ summary: 'Lista todos os usuários' })
   findAll() {
     return this.usuariosService.obterTodos();
@@ -51,14 +48,14 @@ export class UsuariosController {
   }
 
   @Patch(':id')
-  @Roles(UserRole.ADMIN) // Só ADMIN edita (regra de negócio comum)
+  @Roles(UserRole.ADMIN) 
   @ApiOperation({ summary: 'Atualiza dados de um usuário (Apenas Admin)' })
   update(@Param('id') id: string, @Body() atualizarUsuarioDto: AtualizarUsuarioDto) {
     return this.usuariosService.atualizar(id, atualizarUsuarioDto);
   }
 
   @Delete(':id')
-  @Roles(UserRole.ADMIN) // Só ADMIN deleta
+  @Roles(UserRole.ADMIN) 
   @HttpCode(HttpStatus.NO_CONTENT) 
   @ApiOperation({ summary: 'Remove um usuário do sistema (Apenas Admin)' })
   remove(@Param('id') id: string) {
